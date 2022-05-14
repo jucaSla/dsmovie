eab34bbd2e2053dd31969c938a5f2aa9bcdf2fd5
jucaSla <jaomanuelb@gmail.com>
2022-05-13 22:02:19 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -28,9 +28,14 @@ function Listing() {
             });
 
     }, [pageNumber]);
+
+    const handlePageChage = (newPageNumber :number) => {
+        setPageNumber(newPageNumber);
+    }
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChage} />
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
